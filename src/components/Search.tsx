0cdec816b10ee9d0bca7fb69a8979868a9dfbabf
Daniel Victor <danielvictorsilvacardoso@gmail.com>
2023-12-31 03:24:13 -0300

    initial commit

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
 
 import { BsSearch } from 'react-icons/bs';
+
+import classes from './Search.module.css';
 
 type SearchProps = {
     loadUser: (userName: string) => Promise<void>;
@@ -10,16 +12,23 @@ type SearchProps = {
 const Search = ({loadUser}: SearchProps) => {
     const [userName, setUserName] = useState("");
 
+    const hadleKeyDown = (e: KeyboardEvent) => {
+        if(e.key === "Enter") {
+            loadUser(userName);
+        };
+    };
+
     return (
-        <div>
+        <div className={classes.search}>
             <h2>Busque por um usuário</h2>
             <p>Conheça os melhores repositórios</p>
 
-            <div>
+            <div className={classes.search_container}>
                 <input 
                     type="text" 
                     placeholder='Digite o nome do usuário' 
                     onChange={(e) => setUserName(e.target.value)}
+                    onKeyDown={hadleKeyDown}
                 />
                 <button onClick={() => loadUser(userName)}>
                     <BsSearch />
