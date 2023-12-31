f24d6fdc7f23ddf9b40690c86cb01da26e468dea
Daniel Victor <danielvictorsilvacardoso@gmail.com>
2023-12-31 19:28:19 -0300

    initial commit

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,17 @@
 import { UserProps } from "../types/user";
+import { useState } from "react";
 
 import Search from "../components/Search";
 import User from "../components/User";
 import Error from "../components/Error";
+import Loader from "../components/Loader";
 
-import { useState } from "react";
+
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
     const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const loadUser = async (userName: string) => {
         setError(false);
