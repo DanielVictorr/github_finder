0129e3368b3f872691525e12c19bd52c346beda0
Daniel Victor <danielvictorsilvacardoso@gmail.com>
2023-12-31 03:38:44 -0300

    initial commit

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,16 +2,25 @@ import { UserProps } from "../types/user";
 
 import Search from "../components/Search";
 import User from "../components/User";
+import Error from "../components/Error";
 
 import { useState } from "react";
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false);
 
     const loadUser = async (userName: string) => {
+        setError(false);
+        setUser(null)
         const res = await fetch(`https://api.github.com/users/${userName}`);
 
         const data = await res.json();
+
+        if(res.status === 404 ) {
+            setError(true);
+            return;
+        };
 
         console.log(data);
 
@@ -33,6 +42,8 @@ const Home = () => {
             <Search loadUser={loadUser}/>
 
             {user && <User {...user} />}
+
+            {error && <Error />}
 
         </div>
     
